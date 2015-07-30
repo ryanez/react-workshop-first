085e7961f0e4c41876da6797032f58c7b5a6a3cf
ryanez <ryanez@nearsoft.com>
2015-07-29 18:17:51 -0700

    Exercise 06

---
+++ ui/exercise-06.js
@@ -0,0 +1,8 @@
+
+function rockandroll() {
+    var mountNode1 = document.getElementById('content-1'),
+        mountNode2 = document.getElementById('content-2');
+
+    React.render(<Timer frequency="4" />, mountNode1);
+    React.render(<Timer frequency="6" />, mountNode2);
+}
